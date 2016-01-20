d62f232378f9b2415bb8a23041f16f44fb5cf06b
Matt Wagner <mattgwagner@gmail.com>
2016-01-20 10:49:12 -0500

    Add ExceptionLoggingFilter to CertiPay.Common.Web package

---
+++ CertiPay.Common.Web/ExceptionLoggingFilter.cs
@@ -0,0 +1,15 @@
+﻿using CertiPay.Common.Logging;
+using System.Web.Mvc;
+
+namespace CertiPay.Common.Web
+{
+    public sealed class ExceptionLoggingFilter : IExceptionFilter
+    {
+        private static readonly ILog Log = LogManager.GetLogger("UncaughtExceptions");
+
+        public void OnException(ExceptionContext ctx)
+        {
+            Log.ErrorException("Unhandled Exception on Request: " + ctx.HttpContext.Request.Url + " : " + ctx.Exception.Message, ctx.Exception);
+        }
+    }
+}
