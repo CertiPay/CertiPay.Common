98468a121430aab68ffa7e8758307cfc8250d146
titanium007 <jehonathant@gmail.com>
2016-01-25 11:55:17 -0500

    check for cancellation

--- CertiPay.Common.Notifications/Extensions/SmtpExtensions.cs
+++ CertiPay.Common.Notifications/Extensions/SmtpExtensions.cs
@@ -13,6 +13,7 @@ namespace CertiPay.Common.Notifications.Extensions
         /// <summary>
         /// Extension method to have SmtpClient's SendMailAsync respond to a CancellationToken
         /// https://gist.github.com/mattbenic/400e3c039ab8ea3e33aa
+        /// https://msdn.microsoft.com/en-us/library/system.net.mail.smtpclient.sendasynccancel(v=vs.110).aspx
         /// </summary>
         public static async Task SendMailAsync(
             this SmtpClient client,
@@ -23,10 +24,12 @@ namespace CertiPay.Common.Notifications.Extensions
             {
                 client.SendAsyncCancel();
             };
-            using (var reg = token.Register(cancelSend))
-            {
-                await client.SendMailAsync(message);
-            }
+
+            if (!token.IsCancellationRequested)
+                using (var reg = token.Register(cancelSend))
+                {
+                    await client.SendMailAsync(message);
+                }
         }
     }
 }
