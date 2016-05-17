ebd4b4ed2a65a65a083e9191e56fb9690ee96d93
Matt Wagner <mattgwagner@gmail.com>
2016-05-17 14:51:56 -0400

    Update autorollback for nunit3-isms

--- CertiPay.Common.Testing/AutoRollbackAttribute.cs
+++ CertiPay.Common.Testing/AutoRollbackAttribute.cs
@@ -1,6 +1,7 @@
 ﻿namespace CertiPay.Common.Testing
 {
     using NUnit.Framework;
+    using NUnit.Framework.Interfaces;
     using System;
     using System.Transactions;
 
@@ -26,7 +27,14 @@
 
         public int TimeOutInSeconds { get; set; }
 
-        public void BeforeTest(TestDetails testDetails)
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1063:ImplementIDisposableCorrectly")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2213:DisposableFieldsShouldBeDisposed", MessageId = "_scope")]
+        public void Dispose()
+        {
+            // Nothing to do here. TransactionScope is disposed after test completion
+        }
+
+        public void BeforeTest(ITest test)
         {
             var options = new TransactionOptions { IsolationLevel = this.IsolationLevel };
 
@@ -38,16 +46,9 @@
             this._scope = new TransactionScope(this.ScopeOption, options);
         }
 
-        public void AfterTest(TestDetails testDetails)
+        public void AfterTest(ITest test)
         {
             this._scope.Dispose();
-        }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1063:ImplementIDisposableCorrectly")]
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2213:DisposableFieldsShouldBeDisposed", MessageId = "_scope")]
-        public void Dispose()
-        {
-            // Nothing to do here. TransactionScope is disposed after test completion
         }
     }
 }
