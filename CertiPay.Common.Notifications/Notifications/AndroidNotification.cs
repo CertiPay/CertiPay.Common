b5361d004e206eac837b1e2c86bf6fe47efa8820
Matt Wagner <mattgwagner@gmail.com>
2016-07-12 11:53:05 -0400

    Initial add of the android notification model

---
+++ CertiPay.Common.Notifications/Notifications/AndroidNotification.cs
@@ -0,0 +1,11 @@
+﻿namespace CertiPay.Common.Notifications.Notifications
+{
+    public class AndroidNotification : Notification
+    {
+        public static string QueueName { get { return "AndroidNotifications"; } }
+
+        // TODO Android specific properties? Title, Image, Sound, Action Button, Picture, Priority
+
+        // Message => Content
+    }
+}
