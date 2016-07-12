3bb9522fd1c6ad810460892f3aea000b0da39625
Matt Wagner <mattgwagner@gmail.com>
2016-07-12 11:58:00 -0400

    Add title to android notifications object

--- CertiPay.Common.Notifications/Notifications/AndroidNotification.cs
+++ CertiPay.Common.Notifications/Notifications/AndroidNotification.cs
@@ -1,11 +1,18 @@
-﻿namespace CertiPay.Common.Notifications.Notifications
+﻿using System;
+
+namespace CertiPay.Common.Notifications.Notifications
 {
     public class AndroidNotification : Notification
     {
-        public static string QueueName { get { return "AndroidNotifications"; } }
-
-        // TODO Android specific properties? Title, Image, Sound, Action Button, Picture, Priority
+        public static string QueueName { get; } = "AndroidNotifications";
 
         // Message => Content
+
+        /// <summary>
+        /// The subject line of the email
+        /// </summary>
+        public String Title { get; set; }
+
+        // TODO Android specific properties? Image, Sound, Action Button, Picture, Priority
     }
 }
