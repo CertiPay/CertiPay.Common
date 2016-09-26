c69a51b71ae3abfbaf9ff0918dd3ddbd000e36d9
Matt Wagner <mattgwagner@gmail.com>
2016-09-26 10:18:01 -0400

    Use a static constructor for init instead of lazy
    This will let me override behavior in the apps

--- CertiPay.Common/Logging/SerilogManager.cs
+++ CertiPay.Common/Logging/SerilogManager.cs
@@ -8,64 +8,11 @@
 
     internal class SerilogManager : ILog
     {
-        private static readonly Lazy<ILogger> logger = new Lazy<ILogger>(() =>
-        {
-            // Set the defaults so that form data will be logged as info on errors
-            // The default for request logging is Information already
-
-            ApplicationLifecycleModule.RequestLoggingLevel = LogEventLevel.Information;
-            ApplicationLifecycleModule.FormDataLoggingLevel = LogEventLevel.Information;
-            ApplicationLifecycleModule.LogPostedFormData = LogPostedFormDataOption.OnlyOnError;
-
-            // Provide a default rolling file and console configuration for Serilog
-            // User can configure application settings to add on properties or sinks
-            // This ensures that the configuration is done once before use
-
-            return
-                Serilog.Log.Logger =
-                    new LoggerConfiguration()
-                        .ReadFrom.AppSettings()
-
-                        .MinimumLevel.Is(GetLevel(LogManager.LogLevel))
-
-                        .Enrich.FromLogContext()
-                        .Enrich.WithMachineName()
-
-                        // Note: These properties come from the application config file
-                        .Enrich.WithProperty("ApplicationName", LogManager.ApplicationName)
-                        .Enrich.WithProperty("Version", LogManager.Version)
-                        .Enrich.WithProperty("Environment", EnvUtil.Current)
-
-                        // Note: These enrichers grab info off of the HttpRequest.Context if it's available, otherwise are no-ops
-                        // Further, note that these become available via the log template but are not automatically included in the rolling file output
-                        .Enrich.With<HttpRequestIdEnricher>()
-                        .Enrich.With<HttpSessionIdEnricher>()
-                        .Enrich.With<HttpRequestRawUrlEnricher>()
-                        .Enrich.With<HttpRequestUrlReferrerEnricher>()
-                        .Enrich.With<UserNameEnricher>()
-                        .Enrich.With<HttpRequestClientHostIPEnricher>()
-                        .Enrich.With<HttpRequestUserAgentEnricher>()
-
-                        .WriteTo.ColoredConsole()
-
-#if DEBUG
-                        .WriteTo.Sink(new Serilog.Sinks.RollingFile.RollingFileSink(@"c:\logs\out.json", new Serilog.Formatting.Json.JsonFormatter { }, null, null, null))
-#endif
-
-                        .WriteTo.RollingFile(
-                            // Environment, ApplicationName, and date are already in the folder\file name
-                            outputTemplate: "{Timestamp:HH:mm:ss.fff} [{Level}] [{Logger}] {Message}{NewLine}{Exception}",
-                            pathFormat: LogManager.LogPathFormat
-                        )
-
-                        .CreateLogger();
-        });
-
         private readonly ILogger _logger;
 
         internal SerilogManager(String key)
         {
-            _logger = logger.Value.ForContext("Logger", key);
+            _logger = Serilog.Log.ForContext("Logger", key);
         }
 
         internal SerilogManager(ILogger logger)
@@ -117,6 +64,58 @@
                 default:
                     return LogEventLevel.Verbose;
             }
+        }
+
+        static SerilogManager()
+        {
+            // Set the defaults so that form data will be logged as info on errors
+            // The default for request logging is Information already
+
+            ApplicationLifecycleModule.RequestLoggingLevel = LogEventLevel.Information;
+            ApplicationLifecycleModule.FormDataLoggingLevel = LogEventLevel.Information;
+            ApplicationLifecycleModule.LogPostedFormData = LogPostedFormDataOption.OnlyOnError;
+
+            // Provide a default rolling file and console configuration for Serilog
+            // User can configure application settings to add on properties or sinks
+            // This ensures that the configuration is done once before use
+
+            Serilog.Log.Logger =
+                new LoggerConfiguration()
+                    .ReadFrom.AppSettings()
+
+                    .MinimumLevel.Is(GetLevel(LogManager.LogLevel))
+
+                    .Enrich.FromLogContext()
+                    .Enrich.WithMachineName()
+
+                    // Note: These properties come from the application config file
+                    .Enrich.WithProperty("ApplicationName", LogManager.ApplicationName)
+                    .Enrich.WithProperty("Version", LogManager.Version)
+                    .Enrich.WithProperty("Environment", EnvUtil.Current)
+
+                    // Note: These enrichers grab info off of the HttpRequest.Context if it's available, otherwise are no-ops
+                    // Further, note that these become available via the log template but are not automatically included in the rolling file output
+                    .Enrich.With<HttpRequestIdEnricher>()
+                    .Enrich.With<HttpSessionIdEnricher>()
+                    .Enrich.With<HttpRequestRawUrlEnricher>()
+                    .Enrich.With<HttpRequestUrlReferrerEnricher>()
+                    .Enrich.With<UserNameEnricher>()
+                    .Enrich.With<HttpRequestClientHostIPEnricher>()
+                    .Enrich.With<HttpRequestUserAgentEnricher>()
+
+                    .WriteTo.ColoredConsole()
+
+#if DEBUG
+                    .WriteTo.Sink(new Serilog.Sinks.RollingFile.RollingFileSink(@"c:\logs\out.json", new Serilog.Formatting.Json.JsonFormatter { }, null, null, null))
+#endif
+
+                    .WriteTo.RollingFile(
+                        // Environment, ApplicationName, and date are already in the folder\file name
+                        outputTemplate: "{Timestamp:HH:mm:ss.fff} [{Level}] [{Logger}] {Message}{NewLine}{Exception}",
+                        pathFormat: LogManager.LogPathFormat
+                    )
+
+                    .CreateLogger();
         }
     }
 }
