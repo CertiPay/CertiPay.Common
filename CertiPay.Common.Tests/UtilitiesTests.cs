e90c6f59d01c22f742259227b0ff4d3bd8866f68
Matt Wagner <mattgwagner@gmail.com>
2016-11-21 13:19:23 -0500

    remove another local test

--- CertiPay.Common.Tests/UtilitiesTests.cs
+++ CertiPay.Common.Tests/UtilitiesTests.cs
@@ -4,7 +4,7 @@ namespace CertiPay.Common.Tests
 {
     public class UtilitiesTests
     {
-        [Test]
+        [Test, Ignore("This doesn't work on AppVeyor since we patch the properties")]
         public void ShouldMatchVersionOfCaller()
         {
             Assert.AreEqual("0.9.9.local", Utilities.Version<UtilitiesTests>());
