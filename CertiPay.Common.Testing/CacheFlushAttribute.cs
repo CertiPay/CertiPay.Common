884bd4cb3de8373d91bc07e0f196fd7b4e0ba027
Matt Wagner <mattgwagner@gmail.com>
2016-12-06 10:20:55 -0500

    Add attribute to flush the ICache before/after test execution

---
+++ CertiPay.Common.Testing/CacheFlushAttribute.cs
@@ -0,0 +1,27 @@
+﻿using CertiPay.Common.Cache;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using System;
+
+namespace CertiPay.Common.Testing
+{
+    /// <summary>
+    /// Flush the InMemoryCache instance before and after the test execution
+    /// </summary>
+    public class CacheFlushAttribute : Attribute, ITestAction
+    {
+        public ActionTargets Targets { get { return ActionTargets.Test; } }
+
+        private static readonly ICache cache = new InMemoryCache();
+
+        public void AfterTest(ITest test)
+        {
+            cache.Flush().Wait();
+        }
+
+        public void BeforeTest(ITest test)
+        {
+            cache.Flush().Wait();
+        }
+    }
+}
