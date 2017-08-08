67e9764bfac74e0293ac968d9152bbc7975c7e06
Matt Wagner <mattgwagner@gmail.com>
2017-08-08 11:16:36 -0400

    CPO-2531 Handle Development and ASPNETCORE_Environment flags

--- CertiPay.Common/EnvUtil.cs
+++ CertiPay.Common/EnvUtil.cs
@@ -12,10 +12,18 @@ namespace CertiPay.Common
     /// </summary>
     public static class EnvUtil
     {
+        private const String ENV_FLAG = "ASPNETCORE_ENVIRONMENT";
+
+        private const String CONFIG_FLAG = "Environment";
+
         [Flags]
         public enum Environment : byte
         {
+            // ASPNET Core uses Development, Staging, and Production
+            // For our purposes, we're going to consider Local and Development the same
+
             Local = 0,
+            Development = Local,
             Test = 1 << 0,
             Staging = 1 << 1,
             Production = 1 << 2
@@ -29,31 +37,45 @@ namespace CertiPay.Common
         /// <summary>
         /// Returns true if the current environment is marked as Environment.Local
         /// </summary>
-        public static Boolean IsLocal { get { return Environment.Local == Current; } }
+        public static Boolean IsLocal => Current.HasFlag(Environment.Local);
+
+        /// <summary>
+        /// Returns true if the current environment is marked as Environment.Development
+        /// </summary>
+        public static Boolean IsDevelopment => Current.HasFlag(Environment.Development);
 
         /// <summary>
         /// Returns true if the current environment is marked as Environment.Test
         /// </summary>
-        public static Boolean IsTest { get { return Environment.Test == Current; } }
+        public static Boolean IsTest => Current.HasFlag(Environment.Test);
 
         /// <summary>
         /// Returns true if the current environment is marked as Environment.Staging
         /// </summary>
-        public static Boolean IsStaging { get { return Environment.Staging == Current; } }
+        public static Boolean IsStaging => Current.HasFlag(Environment.Staging);
 
         /// <summary>
         /// Returns true if the current environment is marked as Environment.Production
         /// </summary>
-        public static Boolean IsProd { get { return Environment.Production == Current; } }
+        public static Boolean IsProd => Current.HasFlag(Environment.Production);
 
         private static readonly Lazy<Environment> current = new Lazy<Environment>(() =>
         {
             Environment environment = Environment.Local;
 
-            String envString = ConfigurationManager.AppSettings["Environment"].TrimToNull() ?? "Local";
+            // First check the app config, since we might be specifying it there
 
-            if (!Enum.TryParse<Environment>(value: envString, ignoreCase: true, result: out environment))
-                LogManager.GetLogger("EnvUtil").Warn("Environment configuration is invalid. {0}", envString);
+            if (!Enum.TryParse(ConfigurationManager.AppSettings[CONFIG_FLAG], ignoreCase: true, result: out environment))
+            {
+                // Couldn't parse the config, try the environment variables a la ASPNET Core
+
+                if (!Enum.TryParse(System.Environment.GetEnvironmentVariable(ENV_FLAG), ignoreCase: true, result: out environment))
+                {
+                    // Couldn't parse from the environment, fall back to local and warn the user
+
+                    LogManager.GetLogger(nameof(EnvUtil)).Warn("Could not parse environment configuration, defaulting to LOCAL");
+                }
+            }
 
             return environment;
         });
